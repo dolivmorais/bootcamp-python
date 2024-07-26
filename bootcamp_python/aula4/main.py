d83d969a48ae04b522c71f98170c21e186eb7640
dolivmorais <dolivmorais@gmail.com>
2024-07-26 19:42:46 -0300

    carregando pasta aula5 - sem arquivos

--- bootcamp_python/aula4/main.py
+++ bootcamp_python/aula4/main.py
@@ -202,3 +202,10 @@ with open(file=caminho_arquivo, mode= "r", encoding='utf-8') as arquivo:
 
 print(arquivo_csv)
 #%%
+lista_de_numeros = [10.0, 15.5, 7.3, 22.8,-4564.45]
+
+for i in range(len(lista_de_numeros)):
+    for j in range(i +1, len(lista_de_numeros)):
+        if lista_de_numeros[i] > lista_de_numeros[j]:
+            lista_de_numeros[i], lista_de_numeros[j] = lista_de_numeros[j], lista_de_numeros[i]
+print(lista_de_numeros)
--- teste.py
+++
@@ -1,7 +0,0 @@
-lista_de_numeros = [10.0, 15.5, 7.3, 22.8,-4564.45]
-
-for i in range(len(lista_de_numeros)):
-    for j in range(i +1, len(lista_de_numeros)):
-        if lista_de_numeros[i] > lista_de_numeros[j]:
-            lista_de_numeros[i], lista_de_numeros[j] = lista_de_numeros[j], lista_de_numeros[i]
-print(lista_de_numeros)
